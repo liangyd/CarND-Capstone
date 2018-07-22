d9bfa2d007dfa5bd1110b5a23cca33d43d442965
Yiding Liang <liangyd@umich.edu>
2018-07-22 22:15:33 +0000

    complete traffic light detector and waypoint updater

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -10,6 +10,8 @@ from light_classification.tl_classifier import TLClassifier
 import tf
 import cv2
 import yaml
+from scipy.spatial import KDTree
+
 
 STATE_COUNT_THRESHOLD = 3
 
@@ -21,6 +23,9 @@ class TLDetector(object):
         self.waypoints = None
         self.camera_image = None
         self.lights = []
+        
+        self.waypoints_2d=None
+        self.waypoint_tree=None
 
         sub1 = rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
         sub2 = rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
@@ -56,6 +61,9 @@ class TLDetector(object):
 
     def waypoints_cb(self, waypoints):
         self.waypoints = waypoints
+        if not self.waypoints_2d:
+            self.waypoints_2d=[[waypoint.pose.pose.position.x, waypoint.pose.pose.position.y] for waypoint in waypoints.waypoints]
+            self.waypoint_tree=KDTree(self.waypoints_2d)
 
     def traffic_cb(self, msg):
         self.lights = msg.lights
@@ -101,7 +109,11 @@ class TLDetector(object):
 
         """
         #TODO implement
-        return 0
+        x=pose.pose.position.x
+        y=pose.pose.position.y
+        closest_idx=self.waypoint_tree.query([x,y],1)[1]
+        
+        return closest_idx
 
     def get_light_state(self, light):
         """Determines the current color of the traffic light
@@ -113,14 +125,15 @@ class TLDetector(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        if(not self.has_image):
-            self.prev_light_loc = None
-            return False
+        return light.state
+        #if(not self.has_image):
+        #    self.prev_light_loc = None
+        #    return False
 
-        cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
+        #cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
 
         #Get classification
-        return self.light_classifier.get_classification(cv_image)
+        #return self.light_classifier.get_classification(cv_image)
 
     def process_traffic_lights(self):
         """Finds closest visible traffic light, if one exists, and determines its
@@ -131,19 +144,29 @@ class TLDetector(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        light = None
+        line_wp_idx=none
+        closest_light=none
 
         # List of positions that correspond to the line to stop in front of for a given intersection
         stop_line_positions = self.config['stop_line_positions']
         if(self.pose):
-            car_position = self.get_closest_waypoint(self.pose.pose)
+            car_wp_idx = self.get_closest_waypoint(self.pose.pose)
 
-        #TODO find the closest visible traffic light (if one exists)
-
-        if light:
-            state = self.get_light_state(light)
-            return light_wp, state
-        self.waypoints = None
+        # find the closest visible traffic light (if one exists)
+        diff=len(self.waypoints.waypoints)
+        for i, light in enumerate(self.lights):
+            line=stop_line_positions[i]
+            temp_wp_idx=self.get_closest_waypoint(line)
+            d=line_wp_idx-car_wp_idx
+            if d>=0 and d<diff:
+                d=diff
+                closest_light=light
+                line_wp_idx=temp_wp_idx
+                
+        if closest_light:
+            state = self.get_light_state(closest_light)
+            return line_wp_idx, state
+        #self.waypoints = None
         return -1, TrafficLight.UNKNOWN
 
 if __name__ == '__main__':
--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -33,8 +33,8 @@ class WaypointUpdater(object):
         rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
         rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
 
-        # TODO: Add a subscriber for /traffic_waypoint and /obstacle_waypoint below
-        
+        # Add a subscriber for /traffic_waypoint and /obstacle_waypoint below
+        rospy.Subscriber('/traffic_waypoint', Int32, self.traffic_cb)
 
         self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
 
@@ -76,10 +76,34 @@ class WaypointUpdater(object):
         return closest_idx
     
     def publish_waypoints(self, closest_idx):
+        final_lane=self.generate_lane()
+        self.final_waypoints_pub.publish(final_lane)
+    
+    def generate_lane(self):
         lane=Lane()
-        lane.header=self.base_waypoints.header
-        lane.waypoints=self.base_waypoints.waypoints[closest_idx:closest_idx+LOOKAHEAD_WPS]
-        self.final_waypoints_pub.publish(lane)
+        closest_idx=self.get_closest_waypoint_idx()
+        farthest_idx=closest_idx+LOOKAHEAD_WPS
+        base_waypoints=self.base_waypoints.waypoints[closest_idx:closest_idx+LOOKAHEAD_WPS]
+        if self.stopline_waypoint_idx==-1 or self.stopline_waypoint_idx>=farthest_idx:
+            lane.waypoints=base_waypoints
+        else:
+            lane.waypoints=self.decelerate_waypoints(base_waypoints, closest_idx)
+        return lane
+    
+    def decelerate_waypoints(self, base_waypoints, closest_idx):
+        temp=[]
+        for i, wp in enumerate(base_waypoints):
+            p=Waypoint()
+            p.pose=wp.pose
+            stop_idx=max(self.stopline_waypoint_idx-closest_idx-2, 0)
+            dist=self.distance(base_waypoints, i, stop_idx)
+            vel=math.sqrt(2*MAX_DECEL*dist)
+            if vel<1:
+                vel=0
+            p.twist.twist.linear.x=min(vel,wp.twist.twist.linear.x)
+            temp.append(p)
+            
+        return temp
     
     def pose_cb(self, msg):
         # TODO: Implement
@@ -93,8 +117,9 @@ class WaypointUpdater(object):
             self.waypoint_tree=KDTree(self.waypoints_2d)
 
     def traffic_cb(self, msg):
-        # TODO: Callback for /traffic_waypoint message. Implement
-        pass
+        # Callback for /traffic_waypoint message. Implement
+        self.stopline_waypoint_idx=msg.data
+        
 
     def obstacle_cb(self, msg):
         # TODO: Callback for /obstacle_waypoint message. We will implement it later
