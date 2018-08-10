89a760045c337348a394b542de7bbf93f364fa52
Yiding Liang <liangyd@umich.edu>
2018-08-10 06:45:35 +0000

    tensorflow model completed

--- traffic_light_classifier/site_model.py
+++ traffic_light_classifier/site_model.py
@@ -5,6 +5,7 @@ from scipy import misc
 from styx_msgs.msg import TrafficLight
 from distutils.version import LooseVersion
 from PIL import Image
+import cv2
 
 class SiteModel(object):
     def __init__(self, model_checkpoint):
@@ -20,7 +21,7 @@ class SiteModel(object):
 
         self.sess = None
         self.checkpoint = model_checkpoint
-        self.prob_thr = 0.15
+        self.prob_thr = 0.07
         self.TRAFFIC_LIGHT_CLASS = 10
         tf.reset_default_graph()
         
@@ -56,6 +57,40 @@ class SiteModel(object):
                 # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
                 image_np_expanded = np.expand_dims(image_np, axis=0)
                 # Actual detection.
-                (boxes, scores, classes) = sess.run( [detection_boxes, detection_scores, detection_classes], feed_dict={image_tensor: image_np_expanded})
-                
+                (pred_boxes, pred_scores, pred_classes) = sess.run( [detection_boxes, detection_scores, detection_classes], feed_dict={image_tensor: image_np_expanded})
+                pred_boxes = pred_boxes.squeeze()
+                pred_scores = pred_scores.squeeze() # in descreding order
+                pred_classes = pred_classes.squeeze()
+
+                traffic_light = None
+                h, w = img.shape[:2]
+                for i in range(pred_boxes.shape[0]):
+                    box = pred_boxes[i]
+                    score = pred_scores[i]
+
+                    if score < self.prob_thr: continue
+                    if pred_classes[i] != self.TRAFFIC_LIGHT_CLASS: continue
+                    x0, y0 = box[1] * w, box[0] * h
+                    x1, y1 = box[3] * w, box[2] * h
+                    x0, y0, x1, y1 = map(int, [x0, y0, x1, y1])
+                    area = np.abs((x1-x0) * (y1-y0)) / (w*h)
+                    traffic_light = img[y0:y1, x0:x1]
+                    # take the first one - with the most confidence
+                    if traffic_light is not None: break
+
+                if traffic_light is None:
+                    pass
+                else:
+                    brightness = cv2.cvtColor(traffic_light, cv2.COLOR_RGB2HSV)[:,:,-1] 
+                    hs, ws = np.where(brightness >= (brightness.max()-30))
+                    hs_mean = hs.mean()
+                    print(hs_mean)
+                    tl_h = traffic_light.shape[0]
+                    print(tl_h)
+                    if hs_mean / tl_h < 0.4:
+                        return TrafficLight.RED
+                    elif hs_mean / tl_h >= 0.55:
+                        return TrafficLight.GREEN
+                    else:
+                        return TrafficLight.YELLOW
         return TrafficLight.UNKNOWN
--- traffic_light_classifier/test.py
+++ traffic_light_classifier/test.py
@@ -2,8 +2,8 @@ from site_model import SiteModel
 import numpy as np
 import cv2
 
-image = cv2.imread('real_img/G_0.png')
-model=SiteModel('frozen_inference_graph.pb')
+image = cv2.imread('real_img/G_1.png')
+model=SiteModel('remodel/frozen_inference_graph.pb')
 print("testing..")
 result=model.predict(image)
 print(result)
