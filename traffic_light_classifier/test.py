80990999890eab21691bb69e4b5646084cb90315
Yiding Liang <liangyd@umich.edu>
2018-08-10 04:52:30 +0000

    use frozen inference version1.3.0

--- traffic_light_classifier/test.py
+++ traffic_light_classifier/test.py
@@ -3,7 +3,7 @@ import numpy as np
 import cv2
 
 image = cv2.imread('real_img/G_0.png')
-model=SiteModel('models/ssd_mobilenet_v1_coco_2018_01_28/frozen_inference_graph.pb')
+model=SiteModel('frozen_inference_graph.pb')
 print("testing..")
 result=model.predict(image)
 print(result)
